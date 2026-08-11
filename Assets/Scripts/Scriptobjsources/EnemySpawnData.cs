968707818b4d53785df7c9a9ce6591ef490fea67
jguti21597 <jguti0969@gmail.com>
2026-08-11 12:04:47 +0100

    base of game
    Adding game loop manager script, adding base prefab and attributes for enemy, spawn and remove enemy functions also added with initial testing and some enemy movement

---
+++ Assets/Scripts/Scriptobjsources/EnemySpawnData.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New EnemySpawnData", menuName = "Create EnemySpawnData")]
+public class EnemySpawnData : ScriptableObject
+{
+    public GameObject EnemyPrefab;
+    public int EnemyID;
+}
